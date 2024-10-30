fc9b63e8f9747957dc210602d15d885966b80528
TrystanViaud <trystan.viaud@gmail.com>
2024-10-30 16:39:18 +0100

    implementation of a reset zoom button and a button that pans and zoom on Cité Descartes

--- map.js
+++ map.js
@@ -1,21 +1,55 @@
 // Map initialization 
 
 
-
 //ESRI layer basemap
 var basemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+  attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 });
+
+var Base = {
+  "Basemap": basemap
+};
 
 
 // Layer group
-
 
 var NatureBasedSolutions = new L.layerGroup();
 var NatureAgrémentale = new L.LayerGroup();
 var PolitiquesDeLaNature = new L.layerGroup();
 
-// //Cité Descartes
+var overlays = {
+  "Solutions fondées sur la nature": NatureBasedSolutions,
+  "Nature agrémentale": NatureAgrémentale,
+  "Politiques de la nature": PolitiquesDeLaNature
+
+  //   "<img src='./icons/leaf-orange.png'/> <span class='my-layer-item'>NatureBasedSolutions</span>": NatureBasedSolutions,
+  //   "<img src='./icons/leaf-green.png'/> <span class='my-layer-item'>NatureAgrémentale</span>": NatureAgrémentale,
+  //   "<img src='./icons/leaf-red.png'/> <span class='my-layer-item'>PolitiquesDeLaNature</span>": PolitiquesDeLaNature
+};
+
+
+// Icons
+var LeafIcon = L.Icon.extend({
+  options: {
+    shadowUrl: './icons/leaf-shadow.png',
+    iconSize: [38, 95], // size of the icon
+    shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+  }
+});
+
+var greenIcon = new LeafIcon({ iconUrl: './icons/leaf-green.png' }),
+  redIcon = new LeafIcon({ iconUrl: './icons/leaf-red.png' }),
+  orangeIcon = new LeafIcon({ iconUrl: './icons/leaf-orange.png' });
+
+L.icon = function (options) {
+  return new L.Icon(options);
+};
+
+
+// Polygone Cité Descartes
 
 // var citeDescartes = {
 //   "type": "FeatureCollection",
@@ -121,123 +155,149 @@ var PolitiquesDeLaNature = new L.layerGroup();
 // L.geoJSON(citeDescartes).addTo(map);
 
 
-// Icon
 
-var LeafIcon = L.Icon.extend({
-  options: {
-      shadowUrl: './icons/leaf-shadow.png',
-      iconSize:     [38, 95], // size of the icon
-      shadowSize:   [50, 64], // size of the shadow
-      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-      shadowAnchor: [4, 62],  // the same for the shadow
-      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-  }
-});
-
-var greenIcon = new LeafIcon({iconUrl: './icons/leaf-green.png'}),
-    redIcon = new LeafIcon({iconUrl: './icons/leaf-red.png'}),
-    orangeIcon = new LeafIcon({iconUrl: './icons/leaf-orange.png'});
-
-L.icon = function (options) {
-    return new L.Icon(options);
-};
+// // Marker add vimeo
 
 
-// //Marker add
-
-
-var Vilette = L.marker([48.886005, 2.374572], {icon: greenIcon}).addTo(NatureAgrémentale)
-  .bindPopup('<h1>Cours d’eau urbains et baignade en eau libre</h1><iframe src="https://player.vimeo.com/video/910529133?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"vallowfullscreen></iframe>', {
-    maxWidth: 640
-  });
-
-
-var Bassée = L.marker([48.408809, 3.136201], {icon: redIcon}).addTo(PolitiquesDeLaNature)
-  .bindPopup('<h1>Territoire servis, territoires servants : face aux grands projets, quel avenir pour la Bassée ?</h1><iframe src="https://player.vimeo.com/video/910517811?h=d9e60603dc" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"vallowfullscreen></iframe>', {
-    maxWidth: 640
-  });
-
-
-var Morbras = L.marker([48.778111, 2.528954], {icon: redIcon}).addTo(PolitiquesDeLaNature)
-  .bindPopup(
-    '<h1>Le Morbras, une petite rivière urbaine sous pression ou opportunité pour redécouvrir la nature en ville ?</h1>\
-    <h2>Par Laurent Lespez</h2>\
-    <h3>Professeur des Universités en Géographie de l’environnement à l’Université de Paris-Est Créteil et Directeur adjoint du Laboratoire de Géographie Physique du CNRS</h3>\
-  <iframe src="https://player.vimeo.com/video/910353131?h=9700b3b9a8" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"vallowfullscreen></iframe>', {
-    maxWidth: 640
-  });
-
-
-var Bobigny = L.marker([48.899369, 2.434745], {icon: redIcon}).addTo(PolitiquesDeLaNature)
-  .bindPopup('<h1>Les jardins partagés : des nouvelles manières de produire la ville</h1><iframe src="https://player.vimeo.com/video/910385853?h=4c3aa747a5" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"vallowfullscreen></iframe>', {
-    maxWidth: 640
-  });
-
-
-var Compans = L.marker([48.995722, 2.647588], {icon: orangeIcon}).addTo(NatureBasedSolutions)
-  .bindPopup('<h1>Solutions fondées sur la nature pour la gestion durable des ruissellements de voirie</h1><iframe src="https://player.vimeo.com/video/910418837?h=7c1baf663e" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"vallowfullscreen></iframe>', {
-    maxWidth: 640
-  });
-
-
-
-// function onMyFrameLoad() {
-//   alert('myframe is loaded');
-// };
-
-// var FenBrie = L.marker([48.825693, 2.717854], {icon: greenIcon}).addTo(NatureAgrémentale)
-//   .bindPopup('<h1>La nature des zones logistique : Ferrière-en-Brie (Seine-et-Marne)</h1><iframe id="myframe" width="100%" height="=100%" src="https://www.canal-u.tv/chaines/nu/embed/156833?t=0" allowfullscreen style="position:absolute; top:0; left: 0"  onload="onMyFrameLoad(this)"></iframe>', {
+// var Vilette = L.marker([48.886005, 2.374572], { icon: greenIcon }).addTo(NatureAgrémentale)
+//   .bindPopup('<h1>Cours d’eau urbains et baignade en eau libre</h1><iframe src="https://player.vimeo.com/video/910529133?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"vallowfullscreen></iframe>', {
 //     maxWidth: 640
 //   });
 
 
-var FenBrie = L.marker([48.825693, 2.717854], {icon: greenIcon}).addTo(NatureAgrémentale)
-  .bindPopup('<h1>La nature des zones logistique : Ferrière-en-Brie (Seine-et-Marne)</h1><div style="padding-bottom:56.25%; position:relative; display:block; width: 100%"><iframe width="100%" height="100%" src="https://www.canal-u.tv/chaines/nu/embed/156833?t=0" frameborder="0" allowfullscreen="" style="position:absolute; top:0; left: 0"></iframe></div>', 
-    {maxWidth: 640
+// var Bassée = L.marker([48.408809, 3.136201], { icon: redIcon }).addTo(PolitiquesDeLaNature)
+//   .bindPopup('<h1>Territoire servis, territoires servants : face aux grands projets, quel avenir pour la Bassée ?</h1><iframe src="https://player.vimeo.com/video/910517811?h=d9e60603dc" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"vallowfullscreen></iframe>', {
+//     maxWidth: 640
+//   });
+
+
+// var Morbras = L.marker([48.778111, 2.528954], { icon: redIcon }).addTo(PolitiquesDeLaNature)
+//   .bindPopup(
+//     '<h1>Le Morbras, une petite rivière urbaine sous pression ou opportunité pour redécouvrir la nature en ville ?</h1>\
+//     <h2>Par Laurent Lespez</h2>\
+//     <h3>Professeur des Universités en Géographie de l’environnement à l’Université de Paris-Est Créteil et Directeur adjoint du Laboratoire de Géographie Physique du CNRS</h3>\
+//   <iframe src="https://player.vimeo.com/video/910353131?h=9700b3b9a8" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"vallowfullscreen></iframe>', {
+//     maxWidth: 640
+//   });
+
+
+// var Bobigny = L.marker([48.899369, 2.434745], { icon: redIcon }).addTo(PolitiquesDeLaNature)
+//   .bindPopup('<h1>Les jardins partagés : des nouvelles manières de produire la ville</h1><iframe src="https://player.vimeo.com/video/910385853?h=4c3aa747a5" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>', {
+//     maxWidth: 640
+//   });
+
+
+// var Compans = L.marker([48.995722, 2.647588], { icon: orangeIcon }).addTo(NatureBasedSolutions)
+//   .bindPopup('<h1>Solutions fondées sur la nature pour la gestion durable des ruissellements de voirie</h1><iframe src="https://player.vimeo.com/video/910418837?h=7c1baf663e" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"vallowfullscreen></iframe>', {
+//     maxWidth: 640
+//   });
+
+
+//   var FenBrie = L.marker([48.995722, 2.647588], { icon: orangeIcon }).addTo(NatureAgrémentale)
+//   .bindPopup('<h1>La nature des zones logistique : Ferrière-en-Brie (Seine-et-Marne)</h1><iframe src="https://player.vimeo.com/video/910368493?h=88c7953337" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"vallowfullscreen></iframe>', {
+//     maxWidth: 640
+//   });
+
+
+// var LacChamps = L.marker([48.863325, 2.598717], { icon: greenIcon }).addTo(NatureAgrémentale)
+//   .bindPopup('<h1>Plans d’eau urbains et accès à la nature : Le lac de Champs-sur-Marne</h1><iframe src="https://player.vimeo.com/video/870659723?h=e47f679bc6" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"vallowfullscreen></iframe>', {
+//     maxWidth: 640
+//   });
+
+
+
+// // Cité Descartes
+
+
+// var PipiCaca = L.marker([48.839750, 2.588353], { icon: orangeIcon }).addTo(NatureBasedSolutions)
+//   .bindPopup('<h1>Pipi, caca, matières fertiles ? Quand les citadins désertent le tout-à-l’égout</h1><iframe src="https://player.vimeo.com/video/910480155?h=80877d0394" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"vallowfullscreen></iframe>', {
+//     maxWidth: 640
+//   });
+
+
+// var ArbresDePluie = L.marker([48.841776, 2.589672], { icon: orangeIcon }).addTo(NatureBasedSolutions)
+//   .bindPopup('<h1>Jardin et arbres de pluie pour la gestion &agrave; la source des eaux pluviales urbaines, les dispositifs de SenseCity</h1><iframe src="https://player.vimeo.com/video/910567830?h=7cd44aef18" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>', {
+//     maxWidth: 640
+//   });
+
+
+
+// Marker add canal-u
+
+
+var Vilette = L.marker([48.886005, 2.374572], { icon: greenIcon }).addTo(NatureAgrémentale)
+  .bindPopup('<h1>Cours d’eau urbains et baignade en eau libre</h1>\
+    <iframe width="560" height="315" src="https://www.canal-u.tv/chaines/nu/embed/157472?t=0" allowfullscreen></iframe>', {
+    maxWidth: 560
   });
 
 
-
-
-
-
-  // .bindPopup('<h1>Plans d’eau urbains et accès à la nature : Le lac de Champs-sur-Marne</h1><iframe src="https://player.vimeo.com/video/910368493?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"vallowfullscreen></iframe>', {
-  //   maxWidth: 640
-  // });
-
-//
-
-
-  // <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/910368493?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="La nature des zones logistique : Ferrière-en-Brie (Seine-et-Marne)"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
-  
-
-
-var LacChamps = L.marker([48.863325, 2.598717], {icon: greenIcon}).addTo(NatureAgrémentale)
-  .bindPopup('<h1>Plans d’eau urbains et accès à la nature : Le lac de Champs-sur-Marne</h1><iframe src="https://player.vimeo.com/video/870659723?h=e47f679bc6" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"vallowfullscreen></iframe>', {
-    maxWidth: 640
+var Bassée = L.marker([48.408809, 3.136201], { icon: redIcon }).addTo(PolitiquesDeLaNature)
+  .bindPopup('<h1>Territoire servis, territoires servants : face aux grands projets, quel avenir pour la Bassée ?</h1>\
+    <iframe width="560" height="315" src="https://www.canal-u.tv/chaines/nu/embed/157437?t=0" allowfullscreen></iframe>', {
+    maxWidth: 560
   });
 
+
+var Morbras = L.marker([48.778111, 2.528954], { icon: redIcon }).addTo(PolitiquesDeLaNature)
+  .bindPopup(
+    '<h1>Le Morbras, une petite rivière urbaine sous pression ou opportunité pour redécouvrir la nature en ville ?</h1>\
+    <h2>Par Laurent Lespez</h2>\
+    <h3>Professeur des Universités en Géographie de l’environnement à l’Université de Paris-Est Créteil et Directeur adjoint du Laboratoire de Géographie Physique du CNRS</h3>\
+    <iframe width="560" height="315" src="https://www.canal-u.tv/chaines/nu/embed/157439?t=0" allowfullscreen></iframe>', {
+    maxWidth: 560
+  });
+
+
+var Bobigny = L.marker([48.899369, 2.434745], { icon: redIcon }).addTo(PolitiquesDeLaNature)
+  .bindPopup('<h1>Les jardins partagés : des nouvelles manières de produire la ville</h1>\
+    <iframe width="560" height="315" src="https://www.canal-u.tv/chaines/nu/embed/157443?t=0" allowfullscreen></iframe>', {
+    maxWidth: 560
+  });
+
+
+var Compans = L.marker([48.995722, 2.647588], { icon: orangeIcon }).addTo(NatureBasedSolutions)
+  .bindPopup('<h1>Solutions fondées sur la nature pour la gestion durable des ruissellements de voirie</h1>\
+    <iframe width="560" height="315" src="https://www.canal-u.tv/chaines/nu/embed/156515?t=0" allowfullscreen></iframe>', {
+    maxWidth: 560
+  });
+
+var FenBrie = L.marker([48.825693, 2.717854], { icon: greenIcon }).addTo(NatureAgrémentale)
+  .bindPopup('<h1>La nature des zones logistique : Ferrière-en-Brie (Seine-et-Marne)</h1>\
+    <iframe width="560" height="315" src="https://www.canal-u.tv/chaines/nu/embed/156833?t=0" allowfullscreen></iframe>', {
+    maxWidth: 560 
+});
+
+
+
+var LacChamps = L.marker([48.863325, 2.598717], { icon: greenIcon }).addTo(NatureAgrémentale)
+  .bindPopup('<h1>Plans d’eau urbains et accès à la nature : Le lac de Champs-sur-Marne</h1>\
+    <iframe width="560" height="315" src="https://www.canal-u.tv/chaines/nu/embed/157473?t=0" allowfullscreen></iframe>', {
+    maxWidth: 560
+  });
 
 
 // Cité Descartes
 
+  
+var ArbresDePluie = L.marker([48.841776, 2.589672], { icon: orangeIcon }).addTo(NatureBasedSolutions)
+.bindPopup('<h1>Jardin et arbres de pluie pour la gestion &agrave; la source des eaux pluviales urbaines, les dispositifs de SenseCity</h1>\
+  <iframe width="560" height="315" src="https://www.canal-u.tv/chaines/nu/embed/157441?t=0" allowfullscreen></iframe>',  { 
+  maxWidth : 560 
+});
 
-var PipiCaca = L.marker([48.841497, 2.587606], {icon: orangeIcon}).addTo(NatureBasedSolutions)
-  .bindPopup('<h1>Pipi, caca, matières fertiles ? Quand les citadins désertent le tout-à-l’égout</h1><iframe src="https://player.vimeo.com/video/910480155?h=80877d0394" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"vallowfullscreen></iframe>', {
-    maxWidth: 640
+
+var PipiCaca = L.marker([48.839750, 2.588353], { icon: orangeIcon }).addTo(NatureBasedSolutions)
+  .bindPopup('<h1>Pipi, caca, matières fertiles ? Quand les citadins désertent le tout-à-l’égout</h1>\
+    <iframe width="560" height="315" src="https://www.canal-u.tv/chaines/nu/embed/157435?t=0" allowfullscreen></iframe>', {
+    maxWidth: 560
   });
 
 
-var ArbresDePluie = L.marker([48.841776, 2.589672], {icon: orangeIcon}).addTo(NatureBasedSolutions)
-  .bindPopup('<h1>Jardin et arbres de pluie pour la gestion &agrave; la source des eaux pluviales urbaines, les dispositifs de SenseCity</h1><iframe src="https://player.vimeo.com/video/910567830?h=7cd44aef18" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>', {
-    maxWidth: 640
-  });
 
-  // .bindPopup('<h1>Jardin et arbres de pluie pour la gestion &agrave; la source des eaux pluviales urbaines, les dispositifs de SenseCity</h1><iframe width="560" height="315" src="https://www.canal-u.tv/embed/132756" frameborder="0" allow="accelerometer; autoplay; clipboard-write;  gyroscope; picture-in-picture" encrypted-media" allowfullscreen></iframe>',  { 
-  //   maxWidth : 560 
-  // });
 
-    
+// var map
+
 var map = L.map('map', {
   center: [48.840265, 2.584583],
   minZoom: 9,
@@ -247,21 +307,24 @@ var map = L.map('map', {
     NatureBasedSolutions,
     NatureAgrémentale,
     PolitiquesDeLaNature
-    ]
+  ]
 });
 
-var Base = {
-  "Basemap": basemap
-};
 
-var overlays = {
-  "Solutions fondées sur la nature": NatureBasedSolutions,
-  "Nature agrémentale": NatureAgrémentale,
-  "Politiques de la nature": PolitiquesDeLaNature
+// Reset zoom 
 
-//   "<img src='./icons/leaf-orange.png'/> <span class='my-layer-item'>NatureBasedSolutions</span>": NatureBasedSolutions,
-//   "<img src='./icons/leaf-green.png'/> <span class='my-layer-item'>NatureAgrémentale</span>": NatureAgrémentale,
-//   "<img src='./icons/leaf-red.png'/> <span class='my-layer-item'>PolitiquesDeLaNature</span>": PolitiquesDeLaNature
-};
+L.control.resetView({
+    position: "topleft",
+    title: "Reset view",
+    latlng: L.latLng([48.840265, 2.584583]),
+    zoom: 9,
+}).addTo(map);
 
-L.control.layers(Base, overlays, {collapsed:false, hideSingleBase:true, position:"bottomleft"}).addTo(map);
+
+// Cité Descartes
+
+var button = document.getElementById('refreshButton');
+button.addEventListener('click', () => map.flyTo([48.840897, 2.589177], 16));
+
+
+L.control.layers(Base, overlays, { collapsed: false, hideSingleBase: true, position: "bottomleft" }).addTo(map);
